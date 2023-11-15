8d6d8ba261f95426ed9f4174ecfe10ef3d8b2b08
gilangp03 <gilangfajarp.03@gmail.com>
2023-11-15 12:28:05 +0700

    Deploy lagi

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',''
+    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
