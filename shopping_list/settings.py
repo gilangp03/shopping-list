e25fe44a4ca11279480779a4f3225ff53e0cda34
gilangp03 <gilangfajarp.03@gmail.com>
2023-11-15 12:17:32 +0700

    Deploy

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-kx#)qjgjb&4gh29kzkqndfeyd_$nr15#ded3fmbk4q%*l9mpdd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',''
 ]
 
 MIDDLEWARE = [
@@ -124,8 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
